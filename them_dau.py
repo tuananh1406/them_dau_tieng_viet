33d05cdfe2e4b28570c31608b739d0b9766c9c28
htuananh <anhnguyenhuutuan@vccorp.vn>
2021-05-29 15:44:23 +0700

    Thêm hàm xoá dấu tiếng việt để tạo dữ liệu train

--- them_dau.py
+++ them_dau.py
@@ -53,7 +53,28 @@ def save_train(path_train_dir, path_train_file):
                     idx += 1
 
 
+def remove_tone_line(input_string):
+    # Hàm xoá dấu, chuyển tiếng việt có dấu sang không dấu
+    intab_l = "ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđ"
+    intab_u = "ẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸĐ"
+    intab = list(intab_l+intab_u)
+
+    outtab_l = "a"*17 + "o"*17 + "e"*11 + "u"*11 + "i"*5 + "y"*5 + "d"
+    outtab_u = "A"*17 + "O"*17 + "E"*11 + "U"*11 + "I"*5 + "Y"*5 + "D"
+    outtab = outtab_l + outtab_u
+    # Khởi tạo regex tìm kiếm các vị trí nguyên âm có dấu 'ạ|ả|ã|...'
+    r = re.compile("|".join(intab))
+
+    # Dictionary có key-value là từ có dấu-từ không dấu. VD: {'â' : 'a'}
+    replaces_dict = dict(zip(intab, outtab))
+    # Thay thế các từ có dấu xuất hiện trong tìm kiếm của regex bằng từ không dấu tương ứng
+    non_dia_str = r.sub(lambda m: replaces_dict[m.group(0)], input_string)
+    return non_dia_str
+
+
 if __name__ == '__main__':
     is_vn = _check_tieng_viet('tiếng việt là ngôn ngữ của tôi')
     print(is_vn)
-    save_train('output/*/*', 'train_tieng_viet.txt')
+    khong_dau = remove_tone_line('Đi một ngày đàng học 1 sàng khôn')
+    print(khong_dau)
+    # save_train('output/*/*', 'train_tieng_viet.txt')
