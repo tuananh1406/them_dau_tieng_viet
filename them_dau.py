0632ed15f03b14634bbc28f6dd240413e17ef141
htuananh <anhnguyenhuutuan@vccorp.vn>
2021-05-29 15:51:50 +0700

    Sửa lại format hàm save_train cho đúng chuẩn

--- them_dau.py
+++ them_dau.py
@@ -8,7 +8,7 @@ import glob2
 
 
 def _check_tieng_viet(seq):
-    # Một câu sẽ được coi là hợp lệ nếu có các ký tự nằm trong accept_strings
+    '''Một câu sẽ được coi là hợp lệ nếu có các ký tự nằm trong accept_strings'''
 
     # https://realpython.com/python-encodings-guide/
     # List các ký tự hợp lệ trong tiếng Việt
@@ -23,14 +23,15 @@ def _check_tieng_viet(seq):
 
 
 def save_train(path_train_dir, path_train_file):
-    # Lưu file huấn luyện
-    # tạo vòng lặp đi qua toàn bộ các file trong train folder. Kiểm tra câu có thỏa mãn tiêu chuẩn Tiếng Việt không,
-    # đánh index cho câu và lưu đồng thời index và câu có dấu vào train file
+    '''Lưu file huấn luyện
+    tạo vòng lặp đi qua toàn bộ các file trong train folder. Kiểm tra câu có thỏa mãn tiêu chuẩn Tiếng Việt không,
+    đánh index cho câu và lưu đồng thời index và câu có dấu vào train file
+    '''
     idx = 0
     for path in tqdm(glob2.glob(path_train_dir)):
         # Đọc nội dung của các văn bản từ folder output. Content sẽ chứa nhiều row, mỗi row là một json data
-        with open(path, 'r', encoding='utf8') as f:
-            content = f.readlines()
+        with open(path, 'r', encoding='utf8') as raw_data:
+            content = raw_data.readlines()
             for row in content:
                 # Convert row sang json
                 art_json = json.loads(row)
@@ -44,13 +45,14 @@ def save_train(path_train_dir, path_train_file):
                 for seq in art_seqs:
                     if _check_tieng_viet(seq):
                         idx_str = str(idx).zfill(10)
-                    with open(path_train_file, 'a') as f:
-                        f.writelines([idx_str+'\t', seq+'\n'])
+                    with open(path_train_file, 'a') as train_file:
+                        train_file.writelines([idx_str+'\t', seq+'\n'])
                     idx += 1
 
 
 def remove_tone_line(input_string):
-    # Hàm xoá dấu, chuyển tiếng việt có dấu sang không dấu
+    '''Hàm xoá dấu, chuyển tiếng việt có dấu sang không dấu'''
+
     intab_l = "ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđ"
     intab_u = "ẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸĐ"
     intab = list(intab_l+intab_u)
@@ -59,12 +61,12 @@ def remove_tone_line(input_string):
     outtab_u = "A"*17 + "O"*17 + "E"*11 + "U"*11 + "I"*5 + "Y"*5 + "D"
     outtab = outtab_l + outtab_u
     # Khởi tạo regex tìm kiếm các vị trí nguyên âm có dấu 'ạ|ả|ã|...'
-    r = re.compile("|".join(intab))
+    result = re.compile("|".join(intab))
 
     # Dictionary có key-value là từ có dấu-từ không dấu. VD: {'â' : 'a'}
     replaces_dict = dict(zip(intab, outtab))
     # Thay thế các từ có dấu xuất hiện trong tìm kiếm của regex bằng từ không dấu tương ứng
-    non_dia_str = r.sub(lambda m: replaces_dict[m.group(0)], input_string)
+    non_dia_str = result.sub(lambda m: replaces_dict[m.group(0)], input_string)
     return non_dia_str
 
 
